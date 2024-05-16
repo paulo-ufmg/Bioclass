329dac233c8f368c0d45fc1ca89d3470aa062b88
paulo-ufmg <paulo-rocha@ufmg.br>
2024-05-16 07:34:22 -0300

    Implementações para a versão 0.0.2

--- testeClass.py
+++ testeClass.py
@@ -3,17 +3,24 @@ from Bioclass import Bioprof #Outra forma de carregar a class é <import Bioclas
 import os #Importação do módulo para compatibilizar o sistema operacional com o caractere separador de diretório (Windows [\] Linus [/] ...)
 
 #Referência dos arquivos de sequencias (Fasta) a serem importados
-arquivo = 'Lista02' + os.sep + 'dados' + os.sep + 'arquivo_L02E05_file_fasta.txt' 
-arquivo2 = os.sep + 'dados' + os.sep + 'sequencias_spike.fasta.txt'
+arquivo = 'dados' + os.sep + 'L02E05_file_fasta.txt' 
+arquivo2 = 'dados' + os.sep + 'sequencias_spike.fasta.txt'
 
 seq = Bioprof() #Instanciando o objeto da nossa classe
 
 seq.leiaArquivoFasta(arquivo) #Método que carrega as sequências e dados complementares
+seq.leiaArquivoFasta(arquivo2) #Método que carrega as sequências e dados complementares
 
 print("Sequencias:",seq.ids) #Atributo Lista da classe (ids). Contém os Id´s das sequências carregadas
-print(seq.nucleotideos) #Para cada sequencia, totaliza a quantidade de nucleotídeos da sequência
+#print(seq.composicao_total)  #Para cada sequencia, totaliza a quantidade de nucleotídeos ou aminoácidos da sequência
 
 print(seq.get_seqs()) #Método get_seqs() retorna a lista das sequências importadas
 for i in seq.get_seqs():
-    seq.get_info_seq(i) #método get_info_seq(id) retorna dados sobre a sequência
+    seq.ver_info_seq(i) #método get_info_seq(id) retorna dados sobre a sequência
+print("Visualizando a sequenca de id = sp|P59594|SPIKE_SARS")
+print("====================================================")
+seq.ver_info_seq("sp|P59594|SPIKE_SARS")
+print(seq.get_sequencia("sp|P59594|SPIKE_SARS"))    
+print("Composição:")
+print (seq.get_composicao_seq("sp|P59594|SPIKE_SARS"))
 print("------------------------Done!")
